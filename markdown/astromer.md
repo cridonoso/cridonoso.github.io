4ccec084d698c0f171eea00db700052045fced1c
Cristobal Donoso <cridonoso@inf.udec.cl>
2024-10-08 16:09:48 -0300

    astromer content updated

--- markdown/astromer.md
+++ markdown/astromer.md
@@ -1,7 +1,18 @@
 <img title="a title" alt="Alt text" src="https://github.com/cridonoso/cridonoso.github.io/blob/master/figures/astromer/banner.png?raw=true" width=600 height=180>
 
  ## <p style="text-align: left;">A transformer pre-trained model for light curve representation</p>
- 
+
 The Astromer project aims to create a pre-trained foundational model for light curve representation. In collaboration with the Universidad de Concepción and the Institute of Applied Computation Sciences at Harvard, this project was inspired by the success of Large Language Models to develop a novel approach for embedding and representing light curve data.
 
 ### How does it work?
+Similar to text embeddings, Astromer can create embeddings for raw light curves from sequences of observations taken by telescopes in a particular moment (_see image bellow_). 
+ <p style="text-align: left;"><img title="a title" alt="Alt text" src="https://github.com/cridonoso/cridonoso.github.io/blob/master/figures/astromer/lc.png?raw=true" width=480 height=350></p> 
+The shape of the light curve characterizes the astronomical object. Astronomers usually extract statistical and physical features from the time series data to categorize objects into classes. Astromer can do it automatically in a self-supervised way!
+<br><br>
+Astromer optimizes an attention-based encoder to predict masked values that are randomly chosen. 
+<br><br>
+<p style='text-align: right;'><i>
+<b>The model must look at the context</b> to predict the hidden elements. If the model can predict using the information around, then we can say <b>the model has understood the domain.</b>
+</i></p>  
+<br><br>
+
--- pages/astromer.js
+++ pages/astromer.js
@@ -1,15 +1,19 @@
 document.write(`
-    <h1>Astromer</h1>
-    <h2>a transformer pre-trained model for light curve representation</h2>
-    <h3>Description</h3>
-    <p>Astromer is a model ...</p>
-    <p>This is a list: </p>
-    <ul>
-    <li>item</li>
-    <li>item</li>
-    <li>item 2</li>
-    </ul>
-    <pre><code> pip install astromer
-    </code></pre>
+<img title="a title" alt="Alt text" src="https://github.com/cridonoso/cridonoso.github.io/blob/master/figures/astromer/banner.png?raw=true" width=600 height=180>
+
+<h2><p style="text-align: left;">A transformer pre-trained model for light curve representation</p></h2>
+<p>The Astromer project aims to create a pre-trained foundational model for light curve representation. In collaboration with the Universidad de Concepción and the Institute of Applied Computation Sciences at Harvard, this project was inspired by the success of Large Language Models to develop a novel approach for embedding and representing light curve data.</p>
+<h3>How does it work?</h3>
+<p>Similar to text embeddings, Astromer can create embeddings for raw light curves from sequences of observations taken by telescopes in a particular moment (<em>see image bellow</em>). 
+ <p style="text-align: left;"><img title="a title" alt="Alt text" src="https://github.com/cridonoso/cridonoso.github.io/blob/master/figures/astromer/lc.png?raw=true" width=480 height=350></p> 
+The shape of the light curve characterizes the astronomical object. Astronomers usually extract statistical and physical features from the time series data to categorize objects into classes. Astromer can do it automatically in a self-supervised way!
+<br><br>
+Astromer optimizes an attention-based encoder to predict masked values that are randomly chosen. 
+<br><br></p>
+<p style='text-align: right;'><i>
+<b>The model must look at the context</b> to predict the hidden elements. If the model can predict using the information around, then we can say <b>the model has understood the domain.</b>
+</i></p>  
+<br><br>
+
 `);
 
